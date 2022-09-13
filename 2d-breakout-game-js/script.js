6ce4907bac9738d39b75f9b7feaf84d9ba8fde85
liegele <liegele@gmail.com>
2022-09-13 09:58:46 -0300

    Atualizando arquivos

--- 2d-breakout-game-js/script.js
+++ 2d-breakout-game-js/script.js
@@ -144,6 +144,7 @@ function draw() {
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
   if (e.key === "Right" || e.key === "ArrowRight") {
@@ -158,6 +159,14 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if (e.key === "Left" || e.key === "ArrowLeft") {
     leftPressed = false;
+  }
+}
+
+function mouseMoveHandler(e) {
+  // console.log(e.clientX, e.clientY);
+  const relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
   }
 }
 
